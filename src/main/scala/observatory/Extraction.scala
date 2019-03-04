e1070cc582bba2c12de0dddc1d8537c35da99a4c
Thanos Tsounis <ttsounis@sciops.esa.int>
2019-03-04 15:05:09 +0100

    Week 1 1st attempt

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -1,11 +1,27 @@
 package observatory
 
+import java.nio.file.Paths
 import java.time.LocalDate
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+import scala.collection.parallel.ParIterable
+
 
 /**
   * 1st milestone: data extraction
   */
 object Extraction {
+
+  Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
+
+  val spark: SparkSession = SparkSession.builder().appName("Observatory Data Extraction").config("spark.master", "local").getOrCreate()
+
+  val pathAsString: String => String = (r: String) => Paths.get(getClass.getResource(r).toURI).toString
+  val fahrenheitToCelsius: Temperature => Temperature = (f: Temperature) => (f - 32) / 1.8
 
   /**
     * @param year             Year number
@@ -14,7 +30,10 @@ object Extraction {
     * @return A sequence containing triplets (date, location, temperature)
     */
   def locateTemperatures(year: Year, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Temperature)] = {
-    ???
+    val rawStationsData = spark.sparkContext.textFile(pathAsString(stationsFile))
+    val rawTemperaturesData = spark.sparkContext.textFile(pathAsString(temperaturesFile))
+
+    doLolocateTemperatures(year, rawStationsData, rawTemperaturesData).collect().seq
   }
 
   /**
@@ -22,7 +41,19 @@ object Extraction {
     * @return A sequence containing, for each location, the average temperature over the year.
     */
   def locationYearlyAverageRecords(records: Iterable[(LocalDate, Location, Temperature)]): Iterable[(Location, Temperature)] = {
-    ???
+    
+    def sumRecordTemp (acc: Temperature, record: (LocalDate, Location, Temperature)) = acc + record._3
+    def avgTemp(records: ParIterable[(LocalDate, Location, Temperature)]) = records.foldLeft(0.0)(sumRecordTemp) / records.size
+
+    records.par.groupBy(_._2).mapValues(r => avgTemp(r)).seq
+  }
+
+  def doLolocateTemperatures(year: Year, stationsRaw: RDD[String], temperaturesRaw: RDD[String]): RDD[(LocalDate, Location, Temperature)] = {
+
+    val stations: RDD[(StationIdentifier, Location)] = stationsRaw.map(_.split(',')).map(record => ((record(0), record(1)), Location(record(2).toDouble, record(3).toDouble)))
+    val temperatures: RDD[(StationIdentifier, (LocalDate, Temperature))] = temperaturesRaw.map(_.split(',')).map(record => ((record(0), record(1)), (LocalDate.of(year, record(2).toInt, record(3).toInt), fahrenheitToCelsius(record(4).toDouble))))
+
+    stations.join(temperatures).mapValues(value => (value._2._1, value._1, value._2._2)).values
   }
 
 }
--- src/main/scala/observatory/package.scala
+++ src/main/scala/observatory/package.scala
@@ -1,4 +1,5 @@
 package object observatory {
   type Temperature = Double // °C, introduced in Week 1
   type Year = Int // Calendar year, introduced in Week 1
+  type StationIdentifier = (String, String)
 }

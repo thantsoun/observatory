dcf10064584a8619706914f435f9e36a1325536c
Thanos Tsounis <ttsounis@sciops.esa.int>
2019-03-09 18:12:30 +0100

    Milestone 2 done

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,6 +1,7 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+
 import scala.math._
 
 /**
@@ -15,9 +16,39 @@ object Visualization {
   val earthRadius = 6371
   val distanceLimit = 1
 
-  val imageCoordinatesToLocation: (Int, Int) => Location = (x, y) => {
-    val lon = (y - width / 2) * (maxLong / width)
-    val lat = -(x - height / 2) * (maxLat / height)
+  val sumKeys: (Double, (Double, Temperature)) => Temperature = (acc: Double, pair: (Double, Temperature)) => acc + pair._1
+  val sumKeyMultipliedByValue: (Double, (Double, Temperature)) => Temperature = (acc: Double, pair: (Double, Temperature)) => acc + (pair._1 * pair._2)
+
+  val inverseDistanceWeighting: Iterable[(Double, Temperature)] => Temperature = distances => {
+    val inverseDistances = distances.map { case (d, t) => (1.0 / pow(d, 4), t) }
+    val normalizer = inverseDistances.foldLeft(0.0)(sumKeys)
+    inverseDistances.foldLeft(0.0)(sumKeyMultipliedByValue) / normalizer
+  }
+
+  val linearInterpolate: (Double, Int, Int) => Int = (coefficient, lower, upper) => math.round((upper - lower) * coefficient + lower).toInt
+
+  val interpolateColour: (Iterable[(Temperature, Color)], Temperature) => Color = (points, value) => {
+    val (smallerPairs, biggerPairs) = points.partition(_._1 < value)
+
+    if (smallerPairs.isEmpty && biggerPairs.isEmpty) Color(0, 0, 0)
+    else if (smallerPairs.isEmpty) biggerPairs.minBy(_._1)._2
+    else if (biggerPairs.isEmpty) smallerPairs.maxBy(_._1)._2
+    else {
+      val (maxSmallerPair, minBiggerPair) = (smallerPairs.maxBy(_._1), biggerPairs.minBy(_._1))
+      val minToMinMaxDeltaCoeff = (value - maxSmallerPair._1) / (minBiggerPair._1 - maxSmallerPair._1)
+      val (lowerColor, upperColor) = (maxSmallerPair._2, minBiggerPair._2)
+
+      Color(
+        linearInterpolate(minToMinMaxDeltaCoeff, lowerColor.red, upperColor.red),
+        linearInterpolate(minToMinMaxDeltaCoeff, lowerColor.green, upperColor.green),
+        linearInterpolate(minToMinMaxDeltaCoeff, lowerColor.blue, upperColor.blue)
+      )
+    }
+  }
+
+  val pixelToLocation: PixelCoordinates => Location = pixel => {
+    val lon = (pixel._2 - width / 2) * (maxLong / width)
+    val lat = -(pixel._1 - height / 2) * (maxLat / height)
     Location(lat, lon)
   }
 
@@ -42,18 +73,11 @@ object Visualization {
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
 
-    val sumKeys = (acc: Double, pair: (Double, Temperature)) => acc + pair._1 
-    val sumKeyMultByValue = (acc: Double, pair: (Double, Temperature)) => acc + (pair._1 * pair._2)
-    
-    val distances = temperatures.map { case (l, t) => (calculateDistance(location, l), t) }
+    val distances: Iterable[(Double, Temperature)] = temperatures.map { case (l, t) => (calculateDistance(location, l), t) }
 
     val (closestDistance, closestTemperature) = distances.min
     if (closestDistance < distanceLimit) closestTemperature
-    else {
-      val inverseDistances = distances.map { case (d, t) => (1.0 / pow(d, 4), t) }
-      val normalizer = inverseDistances.foldLeft(0.0)(sumKeys)
-      inverseDistances.foldLeft(0.0)(sumKeyMultByValue) / normalizer 
-    }
+    else inverseDistanceWeighting(distances)
   }
 
   /**
@@ -62,7 +86,10 @@ object Visualization {
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
-    ???
+    points.find(_._1 == value) match {
+      case Some((_, color)) => color
+      case None => interpolateColour(points, value)
+    }
   }
 
   /**
@@ -71,7 +98,20 @@ object Visualization {
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
-    ???
+
+    val pixelCoordinates = for {
+      i <- 0 until height
+      j <- 0 until width
+    } yield (i, j)
+
+    val pixels = pixelCoordinates
+      .map(pixelToLocation)
+      .map(predictTemperature(temperatures, _))
+      .map(interpolateColor(colors, _))
+      .map(col => Pixel(col.red, col.green, col.blue, 255)).toArray
+    
+    Image(width, height, pixels)
+    Image.apply(width, height, pixels)
   }
 
 }
--- src/main/scala/observatory/package.scala
+++ src/main/scala/observatory/package.scala
@@ -2,4 +2,5 @@ package object observatory {
   type Temperature = Double // °C, introduced in Week 1
   type Year = Int // Calendar year, introduced in Week 1
   type StationIdentifier = (String, String)
+  type PixelCoordinates = (Int, Int)
 }

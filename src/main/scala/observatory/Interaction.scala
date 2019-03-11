4dd8ae2893a23806371d49227b2ede757ef3c06f
Thanos Tsounis <ttsounis@sciops.esa.int>
2019-03-11 09:09:58 +0100

    Milestone 2 done for real this time

--- src/main/scala/observatory/Interaction.scala
+++ src/main/scala/observatory/Interaction.scala
@@ -1,6 +1,9 @@
 package observatory
 
 import com.sksamuel.scrimage.{Image, Pixel}
+import observatory.Visualization._
+
+import scala.math._
 
 /**
   * 3rd milestone: interactive visualization
@@ -12,31 +15,55 @@ object Interaction {
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
   def tileLocation(tile: Tile): Location = {
-    ???
+    val zoomFactor = pow(2, tile.zoom)
+    val lat = math.atan(math.sinh(math.Pi * (1.0 - 2.0 * tile.y) / zoomFactor))
+    Location(toDegrees(lat), tile.x * 360.0 / zoomFactor - 180.0)
   }
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @param tile Tile coordinates
+    * @param colors       Color scale
+    * @param tile         Tile coordinates
     * @return A 256×256 image showing the contents of the given tile
     */
   def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
-    ???
+    val width = 256
+    val height = 256
+
+    val coords = for {
+      i <- 0 until height
+      j <- 0 until width
+    } yield (i, j)
+
+    val pixels = coords.par
+      .map({case (y, x) => Tile(x + (tile.x * width), y + (tile.y * height), tile.zoom + (math.log(width) / math.log(2)).toInt)})
+      .map(tileLocation)
+      .map(predictTemperature(temperatures, _))
+      .map(interpolateColor(colors, _))
+      .map(col => Pixel(col.red, col.green, col.blue, 255))
+      .toArray
+
+    Image(width, height, pixels)
   }
 
   /**
     * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
-    * @param yearlyData Sequence of (year, data), where `data` is some data associated with
-    *                   `year`. The type of `data` can be anything.
+    *
+    * @param yearlyData    Sequence of (year, data), where `data` is some data associated with
+    *                      `year`. The type of `data` can be anything.
     * @param generateImage Function that generates an image given a year, a zoom level, the x and
     *                      y coordinates of the tile and the data to build the image from
     */
   def generateTiles[Data](
-    yearlyData: Iterable[(Year, Data)],
-    generateImage: (Year, Tile, Data) => Unit
-  ): Unit = {
-    ???
+                           yearlyData: Iterable[(Year, Data)],
+                           generateImage: (Year, Tile, Data) => Unit
+                         ): Unit = {
+    for {
+      zoom <- 0 to 3
+      x <- 0 until pow(2, zoom).toInt
+      y <- 0 until pow(2, zoom).toInt
+      (year, data) <- yearlyData
+    } yield generateImage(year, Tile(y, x, zoom), data)
   }
 
 }

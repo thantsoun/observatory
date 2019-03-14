275200f9c6a9b7a0f48060024c2e31378321779f
Thanos Tsounis <thantsoun@gmail.com>
2019-03-14 20:56:00 +0100

    4t h milestone done

--- src/main/scala/observatory/Manipulation.scala
+++ src/main/scala/observatory/Manipulation.scala
@@ -5,22 +5,33 @@ package observatory
   */
 object Manipulation {
 
+  lazy val computeGrid: Iterable[(Location, Temperature)] => Map[GridLocation, Double] = temperatures =>
+    (for {
+      lat <- -89 to 90
+      lon <- -180 to 179
+    } yield GridLocation(lat, lon) -> Visualization.predictTemperature(temperatures, Location(lat, lon))).toMap
+
   /**
     * @param temperatures Known temperatures
     * @return A function that, given a latitude in [-89, 90] and a longitude in [-180, 179],
     *         returns the predicted temperature at this location
     */
   def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
-    ???
+    computeGrid(temperatures)
   }
 
+
   /**
-    * @param temperaturess Sequence of known temperatures over the years (each element of the collection
-    *                      is a collection of pairs of location and temperature)
+    * @param temperatures Sequence of known temperatures over the years (each element of the collection
+    *                     is a collection of pairs of location and temperature)
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
-  def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
-    ???
+  def average(temperatures: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
+    lazy val gridsPerYear: Iterable[GridLocation => Temperature] = temperatures.map(makeGrid)
+    location => {
+      val temperatures: Iterable[Temperature] = gridsPerYear.map(_ (location))
+      temperatures.sum / temperatures.size
+    }
   }
 
   /**
@@ -29,7 +40,8 @@ object Manipulation {
     * @return A grid containing the deviations compared to the normal temperatures
     */
   def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
-    ???
+    lazy val grid = makeGrid(temperatures)
+    location => grid(location) - normals(location)
   }
 
 
